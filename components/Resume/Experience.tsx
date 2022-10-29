654c6bc265b9340315d90d60c5e9e50c05be3ad3
Emily Ragan <emily.lynn.ragan@gmail.com>
2022-10-28 21:02:50 -0600

    rearrange experience

--- components/Resume/Experience.tsx
+++ components/Resume/Experience.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { ListGroup } from 'react-bootstrap';
 import { IExperience } from './ResumeInterfaces';
 
@@ -8,11 +7,11 @@ export const Experience = (props: IExperience) =>
         <ListGroup.Item>
             <div style={{ display: 'flex', justifyContent: 'space-between' }}>
                 <h3>{props.title}</h3>
-                <h4>{props.employer}</h4>
+                <h4>{props.dates}</h4>
             </div>
             <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <h4>{props.employer}</h4>
                 <h4>{props.location}</h4>
-                <h4>{props.dates}</h4>
             </div>
             <ListGroup variant="flush">
                 {props.bullets.map(b => <ListGroup.Item key={b}>{b}</ListGroup.Item>)}
