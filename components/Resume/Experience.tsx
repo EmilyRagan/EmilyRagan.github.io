d178f0c2e303362cda7a34ba6d340a7bcddb7ccf
Emily Ragan <emily.lynn.ragan@gmail.com>
2022-10-28 21:28:12 -0600

    background color inheritance in preparation for theming

--- components/Resume/Experience.tsx
+++ components/Resume/Experience.tsx
@@ -4,7 +4,7 @@ import { IExperience } from './ResumeInterfaces';
 export const Experience = (props: IExperience) =>
 {
     return (
-        <ListGroup.Item>
+        <ListGroup.Item style={{ backgroundColor: 'inherit' }}>
             <div style={{ display: 'flex', justifyContent: 'space-between' }}>
                 <h3>{props.title}</h3>
                 <h4>{props.dates}</h4>
@@ -14,7 +14,7 @@ export const Experience = (props: IExperience) =>
                 <h4>{props.location}</h4>
             </div>
             <ListGroup variant="flush">
-                {props.bullets.map(b => <ListGroup.Item key={b}>{b}</ListGroup.Item>)}
+                {props.bullets.map(b => <ListGroup.Item key={b} style={{ backgroundColor: 'inherit' }}>{b}</ListGroup.Item>)}
             </ListGroup>
         </ListGroup.Item>
     );
