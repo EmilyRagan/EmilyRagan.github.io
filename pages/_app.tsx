a4ea8d3753f259fe714182080f3de13d7662d07d
Emily Ragan <emily.lynn.ragan@gmail.com>
2023-05-23 19:55:41 -0600

    active link for header nav

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
 import { Nav, Navbar } from 'react-bootstrap';
 import { Github, Linkedin } from 'react-bootstrap-icons';
 import './index.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
+    const router = useRouter();
     return (
         <div style={{ alignItems: 'stretch', display: 'flex', flexDirection: 'column', flexGrow: 1 }}>
             <Navbar bg='dark' sticky='top' variant='dark'>
                 <Navbar.Brand>Emily Ragan</Navbar.Brand>
-                <Nav className='mr-auto'>
+                <Nav className='mr-auto' activeKey={router.route}>
                     <Nav.Item>
                         <Nav.Link href='/'>Resume</Nav.Link>
                     </Nav.Item>
