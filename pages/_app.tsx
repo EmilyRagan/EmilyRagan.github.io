8304de2d33392a8335945ab947f0d4fc1919d1b0
Emily Ragan <emily.lynn.ragan@gmail.com>
2023-05-23 19:48:32 -0600

    correctly use nav.item

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import type { AppProps } from 'next/app';
 import { Nav, Navbar } from 'react-bootstrap';
-import './index.css';
 import { Github, Linkedin } from 'react-bootstrap-icons';
+import './index.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
@@ -10,14 +10,22 @@ function MyApp({ Component, pageProps }: AppProps) {
             <Navbar bg='dark' sticky='top' variant='dark'>
                 <Navbar.Brand>Emily Ragan</Navbar.Brand>
                 <Nav className='mr-auto'>
-                    <Nav.Link href='/'>Resume</Nav.Link>
-                    <Nav.Link href='/seniorproject'>Senior Project</Nav.Link>
-                    <Nav.Link href='https://www.linkedin.com/in/emilylynnragan/'>
-                        <Linkedin />
-                    </Nav.Link>
-                    <Nav.Link href='https://github.com/EmilyRagan'>
-                        <Github />
-                    </Nav.Link>
+                    <Nav.Item>
+                        <Nav.Link href='/'>Resume</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href='/seniorproject'>Senior Project</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href='https://www.linkedin.com/in/emilylynnragan/'>
+                            <Linkedin />
+                        </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href='https://github.com/EmilyRagan'>
+                            <Github />
+                        </Nav.Link>
+                    </Nav.Item>
                 </Nav>
             </Navbar>
             <Component {...pageProps} />
