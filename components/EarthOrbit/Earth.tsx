c57ef13e76535f9c5b7c353223c2e86f1171fda1
Emily Ragan <emily.lynn.ragan@gmail.com>
2023-07-21 20:46:24 -0600

    set up earth

--- components/EarthOrbit/Earth.tsx
+++ components/EarthOrbit/Earth.tsx
@@ -1,10 +1,13 @@
+/* eslint-disable react/no-unknown-property */
+import { useLoader } from '@react-three/fiber';
 import { PropsWithChildren } from 'react';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 export const Earth = (props: PropsWithChildren) =>
 {
-    return <mesh>
-        <sphereGeometry />
-        <meshMatcapMaterial />
+    const gltf = useLoader(GLTFLoader, './assets/Earth_1_12756.glb');
+
+    return <primitive object={gltf.scene}>
         {props.children}
-    </mesh>;
+    </primitive>;
 };
--- pages/earthorbit.tsx
+++ pages/earthorbit.tsx
@@ -1,12 +1,13 @@
 /* eslint-disable react/no-unknown-property */
 import { Canvas } from '@react-three/fiber'
 import { Earth } from '../components/EarthOrbit/Earth';
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
 
 const EarthOrbit = () =>
 {
-    return <Canvas>
-        <ambientLight color={0xfff} />
+    return <Canvas style={{ backgroundColor: '#555' }}>
+        <PerspectiveCamera makeDefault far={200000} fov={75} near={0.1} position={[0, 0, 20000]} />
+        <ambientLight color={0xffffff} />
         <Earth />
         <OrbitControls />
     </Canvas>;
